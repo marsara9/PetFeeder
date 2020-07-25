7653b735f23d6321a6c981c5f7932dcb04cc19ed
Parker Day <marsara9@gmail.com>
2020-07-25 13:10:59 -0400

    Fixing issue with stepper motor causing watchdog reset.

--- code/src/motorcontrol.cpp
+++ code/src/motorcontrol.cpp
@@ -1,17 +1,27 @@
 #include "motorcontrol.h"
 
+#include <Arduino.h>
 #include <Stepper.h>
+#include <math.h>
 
+const int RPM = 10;
 const int STEPS_PER_REVOLUTION = 200;
 Stepper *stepper = new Stepper(STEPS_PER_REVOLUTION, 4, 5, 0, 15);
 
 MotorControl::MotorControl(const int containersPerRotation) {
-    stepper->setSpeed(60);
+    stepper->setSpeed(RPM);
     
     this->containersPerRotation = containersPerRotation;
 }
 
 void MotorControl::feed() {
-    stepper->step(STEPS_PER_REVOLUTION / containersPerRotation);
+    int totalStepsRequired = STEPS_PER_REVOLUTION / containersPerRotation;
+    int stepsPerSecond = (int)(STEPS_PER_REVOLUTION * RPM / 60.0f);
+    int totalLoops = floor(totalStepsRequired / stepsPerSecond);
+    for(int i = 0; i < totalLoops; i++) {
+        stepper->step(stepsPerSecond);
+        delay(0);
+    }
+    stepper->step(totalStepsRequired - stepsPerSecond * totalLoops);
 }
 
