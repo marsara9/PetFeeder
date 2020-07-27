2ac02dccbefff66a47117e6b34b3222e7e909b74
marsara9@gmail.com <marsara9@gmail.com>
2020-07-27 16:08:35 -0400

    Initial checkin for Android app.

---
+++ android/app/src/main/java/com/sdoras/petfeeder/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.sdoras.petfeeder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> true
+            R.id.navigation_dashboard -> true
+            R.id.navigation_notifications -> true
+            else -> false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
---
+++ android/app/src/main/java/com/sdoras/petfeeder/SetupActivity.kt
@@ -0,0 +1,12 @@
+package com.sdoras.petfeeder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+    }
+}
